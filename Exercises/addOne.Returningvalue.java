1021474b7d60a661fcaba01eb0010cf59d02a96c
M1c17 <pictor117@gmail.com>
2019-07-05 19:06:48 -0400

    modification

--- Exercises/addOne.Returningvalue.java
+++
@@ -1,8 +0,0 @@
-public void whatever () {
-    int num;
-    num = addOne(3);       //addOne returns the value 4, which is assigned to num
-}
-
-public int addOne(int x) {  // x gets the value 3
-    return x + 1;           // addOne returns 4
-}
--- Exercises/convert binary to string.java
+++
@@ -1,6 +0,0 @@
-public String toBinary(int n) {
-    if (n <= 1 ) {
-        return String.valueOf(n);
-    }
-    return toBinary(n / 2) + String.valueOf(n % 2);
-}
